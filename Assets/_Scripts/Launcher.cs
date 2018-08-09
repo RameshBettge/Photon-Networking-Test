cecc35e95bde65fe4062894c0a7515310e905f5f
HomePC <gongfumaniac@gmx.de>
2018-08-09 18:37:43 +0200

    Added: NameInputField.cs. UI for setting up a Name and connecting

--- Assets/_Scripts/Launcher.cs
+++ Assets/_Scripts/Launcher.cs
@@ -15,6 +15,12 @@ public class Launcher : Photon.PunBehaviour
     /// </summary>
     [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
     public byte MaxPlayersPerRoom = 4;
+
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    public GameObject controlPanel;
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    public GameObject progressLabel;
+
     #endregion
 
 
@@ -47,9 +53,10 @@ public class Launcher : Photon.PunBehaviour
         PhotonNetwork.logLevel = Loglevel;
     }
 
-    void Start()
+    private void Start()
     {
-        Connect();
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
     }
 
 
@@ -66,7 +73,8 @@ public class Launcher : Photon.PunBehaviour
     /// </summary>
     public void Connect()
     {
-
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
 
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.connected)
@@ -96,6 +104,9 @@ public class Launcher : Photon.PunBehaviour
 
     public override void OnDisconnectedFromPhoton()
     {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+
         Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
     }
 
